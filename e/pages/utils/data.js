a0cdd3c660f5303f4a867d777d743990712d43e6
khanhstech <khanhdesignit@gmail.com>
2022-08-18 13:40:46 +0700

    Khanh
    List-product

---
+++ e/pages/utils/data.js
@@ -0,0 +1,105 @@
+// thông tin sản phẩm để render ra content main
+
+const data = {
+    products: [
+        {
+            name: 'Blueberry cake',
+            slug: 'Blueberry-cake',
+            category: 'Cake',
+            image: '/images/cupcake1.jpg',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 2.5,
+            numReviews: 90,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+        {
+            name: 'Strawberry cake',
+            slug: 'Strawberry-cake',
+            category: 'Cake',
+            image: '/images/cupcake2.png',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 3.5,
+            numReviews: 19,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+        {
+            name: 'Chocolate cake',
+            slug: 'Chocolate-cake',
+            category: 'Cake',
+            image: '/images/cupcake3.png',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 3.5,
+            numReviews: 10,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+        {
+            name: 'Caramel cake',
+            slug: 'Caramel-cake',
+            category: 'Cake',
+            image: '/images/cupcake4.png',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 1.5,
+            numReviews: 19,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+        {
+            name: 'Cheese cake',
+            slug: 'Cheese-cake',
+            category: 'Cake',
+            image: '/images/cupcake5.png',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 4.5,
+            numReviews: 17,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+         {
+            name: 'Chocolateake',
+            slug: 'Chocolateake',
+            category: 'Cake',
+            image: '/images/cupcake6.png',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 2.5,
+            numReviews: 10,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+        {
+            name: 'Milk cake',
+            slug: 'Milk-cake',
+            category: 'Cake',
+            image: '/images/cupcake7.png',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 5.0,
+            numReviews: 14,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+        {
+            name: 'Chocolate cake',
+            slug: 'Chocolate-cake',
+            category: 'Cake',
+            image: '/images/cupcake7.png',
+            price: 5.65,
+            brand: 'Blueberry',
+            rating: 3.0,
+            numReviews: 40,
+            countInStock : 20,
+            description: 'Blueberry cake'
+        },
+        
+    ]
+}
+
+export default data;
