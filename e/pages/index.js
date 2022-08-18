371f63e3caaf9d8bbe1f2acfc37c2a3ae266c67e
khanhstech <khanhdesignit@gmail.com>
2022-08-18 11:38:31 +0700

    Khanh
    install Tailwind

---
+++ e/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../Components/Layout'
+
+export default function Home() {
+  return (
+    <Layout title="Home"/>
+  )
+}
